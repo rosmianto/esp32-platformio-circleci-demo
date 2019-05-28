8495f75933a87c43f43b7fd84a0f064e587f88a3
Rosmianto Aji Saputro <rosmianto.site@gmail.com>
2019-05-28 14:49:38 +0700

    Introduce new bugs

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(13, HIGH);
-  vTaskDelay(500 / portTICK_PERIOD_MS);
+  vTaskDelay(500 / portTICK_PERIOD_MS)
   digitalWrite(13, LOW);
-  vTaskDelay(500 / portTICK_PERIOD_MS);
+  vTaskDelay(500 / portTICK_PERIOD_MS)
 }
