0dc0242136774f17c7464d0396f4a276904cf4fe
Rosmianto Aji Saputro <rosmianto.site@gmail.com>
2019-05-28 11:51:45 +0700

    Add LED blink

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,13 @@
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  digitalWrite(13, LOW);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
 }
